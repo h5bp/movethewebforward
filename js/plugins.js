79282f3a207e48faf0d942551a23b0ae1dbfe94e
Aaron Forsander <aaron.forsander@gmail.com>
2011-11-27 13:25:49 -0600

    Clear twitter search cache when someone clicks a task link.

--- js/plugins.js
+++ js/plugins.js
@@ -38,6 +38,11 @@
       return JSON.parse(value);
     }
 
+    function cacheDel(key) {
+      window.localStorage.removeItem(key);
+      window.localStorage.removeItem(key + '__expires');
+    }
+
     function twitterSearch(query, callback) {
       var searchUrl = 'http://search.twitter.com/search.json?callback=?&q=';
       var results = cacheGet(query);
@@ -79,7 +84,9 @@
       // A URL that will pre-fill a twitter status message.
       var prefillUrl = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(hashtag + ' ' + message);
 
-      linkElem.attr('href', prefillUrl);
+      linkElem.attr('href', prefillUrl).click(function() {
+        cacheDel(hashtag);
+      });
 
       if (hashtag) {
         twitterSearch(hashtag, function(json) {
