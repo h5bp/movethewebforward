b78a7ec38cdec75ab8eb0598022168678d59a890
Paul Irish <paul.irish@gmail.com>
2011-11-29 16:23:38 -0800

    handy push.sh (and a pull for the webserver).

---
+++ pull.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+cd /home/aurgasm/movethewebforward.com
+
+git reset --hard HEAD
+
+git pull origin master
+
---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git push origin master
+
+git push -f origin master:gh-pages
+
