ba45f0dd84716379628176cb090931222a5c5c58
Aaron Forsander <aaron.forsander@gmail.com>
2011-12-10 11:29:31 -0600

    Use a PHP script (needs to run as a cron job) to save twitter avatars to a json file. Update frontend to read from json file.

--- js/plugins.js
+++ js/plugins.js
@@ -6,54 +6,10 @@
       hashtag: undefined,
       message: 'oh yeah!',
       linkSelector: 'a',
-      avatarsSelector: 'div',
-      searchPrefix: '',
       editTweetText: "(edit this tweet as you wish. ♡)"
     };
 
     var options = $.extend({}, defaults, o);
-
-    function cacheSet(key, value, expires) {
-      window.localStorage.setItem(key, JSON.stringify(value));
-      if (expires) {
-        window.localStorage.setItem(key + '__expires', expires);
-      }
-    }
-
-    function cacheGet(key) {
-      var value = window.localStorage.getItem(key);
-      var expires = window.localStorage.getItem(key + '__expires');
-      if (expires && (+new Date) > expires) {
-        return undefined;
-      }
-      return JSON.parse(value);
-    }
-
-    function cacheDel(key) {
-      window.localStorage.removeItem(key);
-      window.localStorage.removeItem(key + '__expires');
-    }
-
-    function twitterSearch(query, callback) {
-      if (options.searchPrefix) {
-        query = options.searchPrefix + query;
-      }
-
-      var searchUrl = 'http://search.twitter.com/search.json?rpp=100&callback=?&q=';
-      var results = cacheGet(query);
-      if (results) {
-        callback(results);
-      }
-      else {
-        $.getJSON(searchUrl + encodeURIComponent(query), function(json) {
-          if (json.results.length) {
-            cacheSet(query, json, (+new Date) + 1000 * 60 * 60);
-          }
-
-          callback(json);
-        });
-      }
-    }
 
     return this.each(function() {
       var $elem = $(this);
@@ -62,11 +18,6 @@
       var linkElem = $.isFunction(options.linkSelector)
             ? options.linkSelector.call($elem)
             : $elem.find(options.linkSelector);
-
-      // The element that will have user avatars appended to it.
-      var avatarsElem = $.isFunction(options.avatarsSelector)
-            ? options.avatarsSelector.call($elem)
-            : $elem.find(options.avatarsSelector);
 
       // The hashtag used to pre-fill twitter and to search twitter for users.
       var hashtag = $.isFunction(options.hashtag)
@@ -86,40 +37,7 @@
       // A URL that will pre-fill a twitter status message.
       var prefillUrl = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(message + ' ' + hashtag + ' ' + editTweetText);
 
-      linkElem.attr('href', prefillUrl).click(function() {
-        cacheDel(options.searchPrefix + hashtag);
-      });
-
-      if (hashtag) {
-        twitterSearch(hashtag, function(json) {
-          // De-dupe.
-          var users = {};
-
-          $.each(json.results, function(i) {
-            if (this.from_user in users) {
-              return;
-            }
-
-            var image = $('<img>', {
-              src: this.profile_image_url,
-              title: this.from_user
-            });
-
-            var link = $('<a/>', {
-            	href: 'http://twitter.com/' + this.from_user
-            });
-
-            avatarsElem.append( link.append( image ) );
-
-            users[this.from_user] = true;
-          });
-
-          if (json.results.length) {
-            avatarsElem.find('p').show();
-          }
-        });
-      }
-
+      linkElem.attr('href', prefillUrl);
     });
   };
 
